744573817216af9932adcb525c199d086711c033
alxkdev <georgalexanderkrah@gmail.com>
2025-10-15 10:24:39 +0200

    Delete ped_animations.lua
    Removed unused ped_animations script – fully commented and unreferenced.
    Unbenutztes ped_animations-Script entfernt – vollständig auskommentiert und nirgends verwendet.

--- ultimate/peds/ped_animations.lua
+++
@@ -1,7 +0,0 @@
-﻿--[[function ped_anims ()
-
-	setPedAnimation( rathausped, "INT_OFFICE", "OFF_Sit_Watch", -1, true, true, false)
-	setPedAnimation ( nuttedance1, "STRIP", "strip_A",-1,true,true,false )
-	setPedAnimation ( nuttedance2, "STRIP", "strip_A",-1,true,true,false )
-	setPedAnimation ( nuttedance3, "STRIP", "strip_A",-1,true,true,false )
-end]]
